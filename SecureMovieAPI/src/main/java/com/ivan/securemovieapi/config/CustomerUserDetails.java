8e2c8ba5724cc334d8b3b79a57974921ea0fba2b
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-08 15:20:59 +0200

    added method for full name and implemented in users.html

--- SecureMovieAPI/src/main/java/com/ivan/securemovieapi/config/CustomerUserDetails.java
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/config/CustomerUserDetails.java
@@ -48,4 +48,8 @@ public class CustomerUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public String getFullName() {
+        return user.getFirstName() + " " + user.getLastName();
+    }
 }
