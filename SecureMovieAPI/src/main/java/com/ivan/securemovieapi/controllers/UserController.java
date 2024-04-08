f12d5633be5f19960a101774026711b78e3d25d9
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-08 12:43:44 +0200

    added register in controller and register_succes.html

--- SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/UserController.java
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.ivan.securemovieapi.controllers;
 
 import com.ivan.securemovieapi.models.User;
+import com.ivan.securemovieapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserRepository repo;
 
     @GetMapping("")
     public String viewHomePage() {
@@ -17,5 +23,11 @@ public class UserController {
     public String registerForm(Model model) {
         model.addAttribute("user", new User());
         return "register_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+        repo.save(user);
+        return "register_success";
     }
 }
