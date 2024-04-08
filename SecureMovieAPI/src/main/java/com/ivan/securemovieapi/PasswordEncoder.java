2fb95fbcaf8f59744ab6ecbcfc166f8f3396ec95
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-08 15:00:07 +0200

    changes in users html and added password encoder class for encode old passwords in mySql

---
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package com.ivan.securemovieapi;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "password";
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println(encodedPassword);
+    }
+}
