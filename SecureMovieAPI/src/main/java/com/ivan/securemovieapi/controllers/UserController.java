8a239e3ea9d4952d765943e775f93da721c5bdde
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-08 12:31:09 +0200

    added register form

--- SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/UserController.java
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.ivan.securemovieapi.controllers;
 
+import com.ivan.securemovieapi.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,11 @@ public class UserController {
     @GetMapping("")
     public String viewHomePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register_form";
     }
 }
