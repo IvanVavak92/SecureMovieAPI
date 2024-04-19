814b7b22e4566e0e189e2b5aa5a654761a1e7c93
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-19 09:25:03 +0200

    added filmdto

---
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/dtos/FilmDTO.java
@@ -0,0 +1,68 @@
+package com.ivan.securemovieapi.dtos;
+
+public class FilmDTO {
+
+    private String title;
+    private String overview;
+    private String posterPath;
+    private double popularity;
+    private double voteAverage;
+    private int voteCount;
+    private String releaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
