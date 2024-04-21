a5391851411f986a651e70cc51d18c4fd8ac9458
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-21 11:29:57 +0200

    set env variable

--- SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/MovieRestController.java
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/MovieRestController.java
@@ -1,6 +1,9 @@
 package com.ivan.securemovieapi.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ivan.securemovieapi.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +15,8 @@ import java.util.List;
 @RestController
 public class MovieRestController {
 
-    private final String API_KEY = "f2aa517f266d39db80bbb00158638f88";
+    @Value("${api.key}")
+    private String apiKey;
     private final String API_URL = "https://api.themoviedb.org/3/discover/movie";
 
     @GetMapping("/getMovies")
@@ -33,7 +37,7 @@ public class MovieRestController {
             // Fetch data until reaching maximum page limit or no more data available
             while (page <= 50) {
                 // Make the HTTP GET request with the provided URL, API key, and page number
-                String response = restTemplate.getForObject(API_URL + "?api_key=" + API_KEY + "&page=" + page, String.class);
+                String response = restTemplate.getForObject(API_URL + "?api_key=" + apiKey + "&page=" + page, String.class);
 
                 // Check if the response is empty
                 if (response != null && !response.isEmpty()) {
