1096767521ea1c62d0a704934432b5f97fa1987c
Ivan Vavak <ivanvavak92@gmail.com>
2024-04-24 09:15:02 +0200

    added more rest endpoints

--- SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/MovieRestController.java
+++ SecureMovieAPI/src/main/java/com/ivan/securemovieapi/controllers/MovieRestController.java
@@ -14,56 +14,53 @@ public class MovieRestController {
 
     @Value("${api.key}")
     private String apiKey;
-    private final String API_URL = "https://api.themoviedb.org/3/discover/movie";
+    private final String BASE_API_URL = "https://api.themoviedb.org/3/";
 
-    @GetMapping("/getMovies")
-    public String getMovies() {
-        // Create a RestTemplate instance
+    private String fetchDataFromApi(String endpoint) {
         RestTemplate restTemplate = new RestTemplate();
-
-        // Initialize ObjectMapper for JSON parsing and serialization
         ObjectMapper objectMapper = new ObjectMapper();
 
-        // Initialize list to store JSON objects
         List<Object> jsonResponseList = new ArrayList<>();
 
-        // Set initial page number
         int page = 1;
 
         try {
-            // Fetch data until reaching maximum page limit or no more data available
-            while (page <= 50) {
-                // Make the HTTP GET request with the provided URL, API key, and page number
-                String response = restTemplate.getForObject(API_URL + "?api_key=" + apiKey + "&page=" + page, String.class);
+            while (page <= 10) {
+                String response = restTemplate.getForObject(BASE_API_URL + endpoint + "?api_key=" + apiKey + "&page=" + page, String.class);
 
-                // Check if the response is empty
                 if (response != null && !response.isEmpty()) {
-                    // Parse JSON response into an object
                     Object jsonResponse = objectMapper.readValue(response, Object.class);
-
-                    // Add parsed JSON object to the list
                     jsonResponseList.add(jsonResponse);
-
-                    // Increment page number for the next request
                     page++;
                 } else {
-                    // No more data available, break out of the loop
                     break;
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace(); // Handle exceptions
+            e.printStackTrace();
         }
 
         try {
-            // Serialize the list of JSON objects into a single JSON string
-            String jsonResponseString = objectMapper.writeValueAsString(jsonResponseList);
-
-            // Return the concatenated responses
-            return jsonResponseString;
+            return objectMapper.writeValueAsString(jsonResponseList);
         } catch (Exception e) {
             e.printStackTrace();
-            return "{}"; // Return an empty JSON object if serialization fails
+            return "{}";
         }
     }
+
+    @GetMapping("/getMovies")
+    public String getMovies() {
+        return fetchDataFromApi("discover/movie");
+    }
+
+    @GetMapping("/getTopRatedMovies")
+    public String getTopRatedMovies() {
+        return fetchDataFromApi("movie/top_rated");
+    }
+
+    @GetMapping("/trending")
+    public String trending() {
+        return fetchDataFromApi("trending/all/day");
+    }
 }
+
